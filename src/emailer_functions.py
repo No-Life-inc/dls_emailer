162f54f7703e37bd791f9ec14e3699a715af462f
Morten Bendeke <mbendeke@hotmail.com>
2024-04-13 20:21:45 +0200

    simplified callback functions

--- src/emailer_functions.py
+++ src/emailer_functions.py
@@ -1,14 +1,6 @@
 
 import json
 
-def create_comment_message(email, story_title):
-    # Create a message for a new comment
-    return json.dumps({'email': email, 'story_title': story_title})
-
-def create_password_change_message(email):
-    # Create a message for a password change
-    return json.dumps({'email': email})
-
 def send_email(email, subject, body):
     # Send an email to the user
     print(f"Sending email to {email} with subject {subject} and body {body}")
@@ -32,7 +24,7 @@ def callback_new_comments(ch, method, properties, body):
 def callback_password_changes(ch, method, properties, body):
     # Parse the message
     message = json.loads(body)
-    email = message['email']
+    email = message['user']['email']
 
     # Create the email subject and body
     subject = "Password change"
